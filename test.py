b4c7ba6ee360d9425ede6f6b130e9e0b9b412488
Tobbinloggin <53196506+Tobbinloggin@users.noreply.github.com>
2020-06-21 07:27:39 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import os.path
 import glob
 import cv2
@@ -10,8 +11,12 @@ model_path = sys.argv[1]  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
+list = os.listdir('LR/') # dir is your directory path
+number_files = len(list)
+intRemaining = number_files
 test_img_folder = 'LR/*'
 results_folder = 'results/*'
+
 
 model = arch.RRDB_Net(3, 3, 64, 23, gc=32, upscale=int(sys.argv[2]), norm_type=None, act_type='leakyrelu', \
                         mode='CNA', res_scale=1, upsample_mode='upconv')
@@ -21,20 +26,24 @@ for k, v in model.named_parameters():
     v.requires_grad = False
 model = model.to(device)
 
+print("There are " + str(number_files) + " files.")
+
 print('Model path {:s}. \nTesting...'.format(model_path))
 
 idx = 0
 
 for path in glob.glob(test_img_folder):
   skip = 0
-  if int(sys.argv[3]) == 0:
+  if int(sys.argv[3]) == 1:
     for result_images in glob.glob(results_folder):
-      if os.path.splitext(os.path.basename(result_images))[0][0:-4] == os.path.splitext(os.path.basename(path))[0]:
+      if os.path.splitext(os.path.basename(result_images))[0] == os.path.splitext(os.path.basename(path))[0]:
         skip = 1
+        idx += 1
+        print(str(idx) + '/' + str(number_files) + ' skipping: ' + os.path.splitext(os.path.basename(path))[0])
   if skip == 0 and int(sys.argv[3]) == 1:
     idx += 1
     base = os.path.splitext(os.path.basename(path))[0]
-    print(idx, base)
+    print(str(idx) + '/' + str(number_files) + ' ' + path)
     # read image
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
@@ -45,11 +54,11 @@ for path in glob.glob(test_img_folder):
     output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
-    cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+    cv2.imwrite('results/{:s}.png'.format(base), output)
   if int(sys.argv[3]) == 0:
     idx += 1
     base = os.path.splitext(os.path.basename(path))[0]
-    print(idx, base)
+    print(str(idx) + '/' + str(number_files) + ' ' + path)
     # read image
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
