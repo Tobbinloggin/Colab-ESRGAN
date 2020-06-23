db0d37c7a744c5c71c7bc2451320dd54aee53a3d
Tobbinloggin <53196506+Tobbinloggin@users.noreply.github.com>
2020-06-23 07:23:17 -0400

    time per frame update

--- test.py
+++ test.py
@@ -6,17 +6,16 @@ import cv2
 import numpy as np
 import torch
 import architecture as arch
-
+import time
+#b.py models/1x_Fatality_DeBlur_270000_G.pth 1 1
 model_path = sys.argv[1]  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
-list = os.listdir('LR/') # dir is your directory path
-number_files = len(list)
+number_files = len(os.listdir('LR/'))
 intRemaining = number_files
 test_img_folder = 'LR/*'
 results_folder = 'results/*'
-
 
 model = arch.RRDB_Net(3, 3, 64, 23, gc=32, upscale=int(sys.argv[2]), norm_type=None, act_type='leakyrelu', \
                         mode='CNA', res_scale=1, upsample_mode='upconv')
@@ -32,6 +31,7 @@ print('Model path {:s}. \nTesting...'.format(model_path))
 
 idx = 0
 
+total_time_start = time.time()
 for path in glob.glob(test_img_folder):
   skip = 0
   if int(sys.argv[3]) == 1:
@@ -41,6 +41,7 @@ for path in glob.glob(test_img_folder):
         idx += 1
         print(str(idx) + '/' + str(number_files) + ' skipping: ' + os.path.splitext(os.path.basename(path))[0])
   if skip == 0 and int(sys.argv[3]) == 1:
+    start = time.time()
     idx += 1
     base = os.path.splitext(os.path.basename(path))[0]
     print(str(idx) + '/' + str(number_files) + ' ' + path)
@@ -55,7 +56,14 @@ for path in glob.glob(test_img_folder):
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
     cv2.imwrite('results/{:s}.png'.format(base), output)
+    end = time.time()
+    total_time_end = time.time()
+    time_per_frame = end - start
+    total_elapsed_time = total_time_end - total_time_start
+    est_duration = number_files * time_per_frame
+    print('********** Time per frame: ' + str(time_per_frame) + 's Time left: ' + str(est_duration - total_elapsed_time) + 's **********')
   if int(sys.argv[3]) == 0:
+    start = time.time()
     idx += 1
     base = os.path.splitext(os.path.basename(path))[0]
     print(str(idx) + '/' + str(number_files) + ' ' + path)
@@ -70,3 +78,9 @@ for path in glob.glob(test_img_folder):
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
     cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+    end = time.time()
+    total_time_end = time.time()
+    time_per_frame = end - start
+    total_elapsed_time = total_time_end - total_time_start
+    est_duration = number_files * time_per_frame
+    print('********** Time per frame: ' + str(time_per_frame) + 's Time left: ' + str(est_duration - total_elapsed_time) + 's **********')
