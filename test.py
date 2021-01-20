7eff44303f64ed36a20ab4bba479f630c776063f
Tobbinloggin <53196506+Tobbinloggin@users.noreply.github.com>
2021-01-20 16:37:30 -0500

    New features
    Implemented:
    - Progress bar
    - Percent done
    - Estimated Hour(s)

--- test.py
+++ test.py
@@ -7,13 +7,35 @@ import numpy as np
 import torch
 import architecture as arch
 import time
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 #b.py models/1x_Fatality_DeBlur_270000_G.pth 1 1
 model_path = sys.argv[1]  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
 number_files = len(os.listdir('LR/'))
-intRemaining = number_files
 test_img_folder = 'LR/*'
 results_folder = 'results/*'
 
@@ -44,7 +66,8 @@ for path in glob.glob(test_img_folder):
     start = time.time()
     idx += 1
     base = os.path.splitext(os.path.basename(path))[0]
-    print(str(idx) + '/' + str(number_files) + ' ' + path)
+    printProgressBar(idx, number_files, prefix = 'Progress:', suffix = 'Complete\n', length = 83)
+    print(str(idx) + '/' + str(number_files) + ' ' + path) #here
     # read image
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
@@ -61,12 +84,13 @@ for path in glob.glob(test_img_folder):
     time_per_frame = end - start
     total_elapsed_time = total_time_end - total_time_start
     est_duration = number_files * time_per_frame
-    print('********** Time per frame: ' + str(time_per_frame) + 's Time left: ' + str(est_duration - total_elapsed_time) + 's **********')
+    print('********** Time per frame: ' + str(time_per_frame) + 's Time left: ' + str(est_duration - total_elapsed_time) + 's / ' + str((est_duration - total_elapsed_time) / 3600) +'h **********')
   if int(sys.argv[3]) == 0:
     start = time.time()
     idx += 1
     base = os.path.splitext(os.path.basename(path))[0]
-    print(str(idx) + '/' + str(number_files) + ' ' + path)
+    printProgressBar(idx, number_files, prefix = 'Progress:', suffix = 'Complete\n', length = 83)
+    print(str(idx) + '/' + str(number_files) + ' ' + path) #here
     # read image
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
@@ -83,4 +107,4 @@ for path in glob.glob(test_img_folder):
     time_per_frame = end - start
     total_elapsed_time = total_time_end - total_time_start
     est_duration = number_files * time_per_frame
-    print('********** Time per frame: ' + str(time_per_frame) + 's Time left: ' + str(est_duration - total_elapsed_time) + 's **********')
+    print('********** Time per frame: ' + str(time_per_frame) + 's Time left: ' + str(est_duration - total_elapsed_time) + 's / ' + str((est_duration - total_elapsed_time) / 3600) +'h **********')
